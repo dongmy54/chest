c3a4d2d766bd15959dce1dffce5072b23930fe84
dongmy54 <615760433@qq.com>
2017-12-31 16:01:12 +0800

    数据导出

--- Gemfile
+++ Gemfile
@@ -51,6 +51,10 @@ gem 'jquery-ui-rails', '6.0.1'
 gem 'kaminari', '1.1.1'              # 用于分页 类似will_paginate
 gem 'ransack', '1.8.4' # 做模糊匹配，它是通过遍历做到的，数据量大时 需用 Elasticsearch 做
 gem 'paper_trail', '8.1.2' # 做版本管控
+# 下面三个 导出excle格式
+gem 'rubyzip', '1.0.0'
+gem 'axlsx', '2.0.1'
+gem 'axlsx_rails', '0.5.1'
 
 group :development, :test do
   gem 'rspec-rails', '3.5.2'
--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class Admin::EventRegistrationsController < AdminController
 
   before_action :find_event
@@ -35,7 +36,25 @@ class Admin::EventRegistrationsController < AdminController
     # 根据id 筛选资料
     if params[:registration_id].present?
       @registrations = @registrations.where( :id => params[:registration_id].split(",") )
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {
+        @registrations = @registrations.reorder("id ASC")
+        csv_string = CSV.generate do |csv|
+          # csv 中装的是二维数组
+          csv << ["报名ID", "票种", "姓名", "状态", "Email", "报名时间"]
+          @registrations.each do |r|
+            csv << [r.id, r.ticket.name, r.name, t(r.status, :scope => "registration.status"), r.email, r.created_at]
+          end
+        end
+        send_data csv_string, :filename => "#{@event.friendly_id}-registrations-#{Time.now.to_s(:number)}.csv"
+      }
+      
+      format.xlsx
     end 
+        
   end
 
   def destroy
