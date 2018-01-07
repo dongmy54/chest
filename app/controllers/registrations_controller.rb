b4f74745b35e6df4d0a8e121e4f1341650376ce0
dongmy54 <615760433@qq.com>
2018-01-07 10:58:18 +0800

    设定等待时间的异步

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < ApplicationController
 
   before_action :find_event
+  before_action :set_pending_registration, :only => [:step1, :step1_update, :step2, :step2_update, :step3, :step3_update]
 
   def new
   end
@@ -13,6 +14,8 @@ class RegistrationsController < ApplicationController
     @registration.current_step = 1
 
     if @registration.save
+      # set 是sidkekiq中的设置时间方法
+      CheckRegistrationJob.set( wait: 1.minutes ).perform_later(@registration.id)
       redirect_to step2_event_registration_path(@event, @registration)
     else
       flash.now[:alert] = @registration.errors[:base].join("、")
@@ -25,11 +28,9 @@ class RegistrationsController < ApplicationController
   end
 
   def step1
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step1_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.current_step = 1
 
     if @registration.update(registration_params)
@@ -40,11 +41,9 @@ class RegistrationsController < ApplicationController
   end
 
   def step2
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step2_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.current_step = 2
 
     if @registration.update(registration_params)
@@ -55,11 +54,9 @@ class RegistrationsController < ApplicationController
   end
 
   def step3
-    @registration = @event.registrations.find_by_uuid(params[:id])
   end
 
   def step3_update
-    @registration = @event.registrations.find_by_uuid(params[:id])
     @registration.status = "confirmed"
     @registration.current_step = 3
 
@@ -83,6 +80,14 @@ class RegistrationsController < ApplicationController
 
   def find_event
     @event = Event.find_by_friendly_id(params[:event_id])
+  end
+
+  def set_pending_registration
+    @registration = @event.registrations.find_by_uuid(params[:id])
+    if @registration.status == "cancalled"
+      flash[:alert] = "请重新报名"
+      redirect_to event_path(@event)
+    end
   end
 
 end
